a2e1497b388faa90ca0f40fcaf910b53950860e8
Harmeet Matharoo <hmjatt@gmail.com>
2022-07-16 20:17:03 -0600

    added operations to `ArithmaticExpressions` class

--- Arithmetic Expressions/src/ArithmaticExpressions.java
+++ Arithmetic Expressions/src/ArithmaticExpressions.java
@@ -1,4 +1,28 @@
 
 public class ArithmaticExpressions {
+	public static void main(String[] args) {
+		  
+		 // expression =     operands & operators
+		 // operands =  values, variables, numbers, quantity
+		 // operators = + - * / % 
 
+		
+		int friends = 10;
+		
+		friends = friends + 1;
+		System.out.println(friends);
+		
+		friends = friends - 1;
+		System.out.println(friends);
+		
+		friends = friends * 2;
+		System.out.println(friends);
+		
+		friends = friends / 5;
+		System.out.println(friends);
+		
+		friends = friends % 3;
+		System.out.println(friends);
+
+	}
 }
