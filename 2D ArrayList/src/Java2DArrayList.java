6982157a8e92a95675b9d10713d8b684eec9c226
Harmeet Matharoo <hmjatt@gmail.com>
2022-08-23 22:45:49 -0600

    Created `Java 2D ArrayList 📜` Project

---
+++ 2D ArrayList/src/Java2DArrayList.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Java2DArrayList {
+	public static void main(String[] args) {
+		// 2D ArrayList = dynamic list of lists and size of lists can be changed during
+		// runtime
+		ArrayList<ArrayList<String>> groceryList = new ArrayList();
+		
+		ArrayList<String> bakeryList = new ArrayList();
+		bakeryList.add("pasta");
+		bakeryList.add("garlic bread");
+		bakeryList.add("donuts");
+		
+		ArrayList<String> produceList = new ArrayList();
+		produceList.add("tomatoes");
+		produceList.add("zucchini");
+		produceList.add("peppers");
+		
+		ArrayList<String> drinksList = new ArrayList();
+		drinksList.add("soda");
+		drinksList.add("coffee");
+		
+		groceryList.add(bakeryList);
+		groceryList.add(produceList);
+		groceryList.add(drinksList);
+		
+		System.out.println(groceryList.get(2).get(1));
+		
+	}
+}
