b2e7af56ff4df8401bebf44ae89cbd648d51b9a8
Harmeet Matharoo <hmjatt@gmail.com>
2022-08-30 22:09:57 -0600

    Created `Final keyword ⛔` project

---
+++ Final Keyword/src/FinalKeyword.java
@@ -0,0 +1,8 @@
+
+public class FinalKeyword {
+	public static void main(String[] args) {
+		final double PI = 3.14159;
+		//PI = 4;  //You can't change a final variable
+		System.out.println(PI);
+	}
+}
