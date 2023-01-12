7f2414ca2d6a6ac2f41e5141883a1b85cee3d82a
Harmeet Matharoo <hmjatt@gmail.com>
2023-01-11 22:55:58 -0500

    Java Objects project - done

--- Java Objects/src/Main.java
+++ Java Objects/src/Main.java
@@ -1,4 +1,18 @@
 
 public class Main {
 
+	public static void main(String[] args) {
+		
+		Car myCar = new Car();
+		Car myCar1= new Car();
+		Car myCar2 = new Car();
+		
+		
+		System.out.println(myCar1.make);
+		System.out.println(myCar1.model);
+		System.out.println();
+		System.out.println(myCar2.make);
+		System.out.println(myCar2.model);
+		myCar.drive();
+	}
 }
